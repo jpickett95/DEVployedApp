ed8b9917a7236916be095278734866c70e131a0f
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-09-21 04:48:03 -0400

    Made main activity which will host the swiping job postings & the Profile Page to host user information;

--- app/src/main/java/com/example/devployedapp/MainActivity.java
+++ app/src/main/java/com/example/devployedapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.devployedapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button menuButton = (Button) findViewById(R.id.menuButton);
+        menuButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfilePage.class));
+            }
+        });
+
+        ImageButton backButton_main = (ImageButton) findViewById(R.id.backButton_main);
+        backButton_main.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/devployedapp/ProfilePage.java
+++ app/src/main/java/com/example/devployedapp/ProfilePage.java
@@ -2,7 +2,11 @@ package com.example.devployedapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProfilePage extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class ProfilePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        ImageButton backButton_profile = (ImageButton) findViewById(R.id.backButton_profile);
+        backButton_profile.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
