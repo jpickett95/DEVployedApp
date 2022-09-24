daaa01cc5cde8d80da870966ad19766d49eccd87
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2022-09-23 20:08:26 -0500

    Added a new TextView object and made it "gone" so it wouldn't take up space in the layout. Original plan was to dynamically create new instances of the TextView when entering new skills/experiences and fitting that into the layout.
    Currently thinking use a popup to ask for input for skills/experiences (very much like popup_filters), and perhaps make 1 table/grid each underneath Skills and Experiences labels, make those tables gone, and have them become visible & populated once user has clicked the completed button on addSkills popup.

--- app/src/main/java/com/example/devployedapp/ProfilePage.java
+++ app/src/main/java/com/example/devployedapp/ProfilePage.java
@@ -1,19 +1,26 @@
 package com.example.devployedapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class ProfilePage extends AppCompatActivity {
+    //public TextView enterSkillTextview = (TextView) findViewById(R.id.skillTextView);
+    //LinearLayout linearLayout = findViewById(R.id.profilePage);
+
+    Dialog skillsDialog; //to create popup to add Skills & Experiences
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
 
         ImageButton backButton_profile = (ImageButton) findViewById(R.id.backButton_profile);
         backButton_profile.setOnClickListener(new View.OnClickListener(){
@@ -21,5 +28,33 @@ public class ProfilePage extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        Button addSkillButton = (Button) findViewById(R.id.addSkillButton2);
+        addSkillButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+          }
+        });
+        skillsDialog = new Dialog(this);
     }
+    public void AddSkills(View v){
+        FloatingActionButton doneAddingButton;
+        skillsDialog.setContentView(R.layout.skills_experiences);
+        doneAddingButton = (FloatingActionButton) skillsDialog.findViewById(R.id.floatingActionButton_doneAdding);
+        doneAddingButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                skillsDialog.dismiss();
+            }
+        });
+        skillsDialog.show();
+    }
+
+ /*   public void addNew(){
+        View view = getLayoutInflater().inflate(R.layout.skills_experiences,null);
+        //TextView newestSkill = new TextView(enterSkillTextview.getContext());
+        linearLayout.addView(view);
+
+    }*/
 }
