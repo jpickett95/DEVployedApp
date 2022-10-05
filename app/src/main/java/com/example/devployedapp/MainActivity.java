2f045a07f8e008c1553b1e4f04bf172ae9ac664e
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-10-05 16:25:50 -0400

    Used SwipeCards library from github to get the job card layout working; will now need to customize the adapter so job information is displayed;

--- app/src/main/java/com/example/devployedapp/MainActivity.java
+++ app/src/main/java/com/example/devployedapp/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.devployedapp;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // For Job Swipe Cards
+    private ArrayList<String> al; // List of cards' names
+    private ArrayAdapter<String> arrayAdapter;
+    private int i; // for onAdapterAboutToEmpty()
 
     Dialog filtersDialog; // For filters popup window on main activity
 
@@ -19,6 +30,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // For SwipeCards until line (90) ***EDIT***
+        al = new ArrayList<>();
+            // Each 'add' is the name of the card
+        al.add("php");
+        al.add("c");
+        al.add("python");
+        al.add("java");
+        al.add("html");
+        al.add("c++");
+        al.add("css");
+        al.add("javascript");
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.swipecards_item, R.id.helloText, al );
+        // arrayAdapter.notifyDataSetChanged(); must be called after adding new items to the 'al' list from this point
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+
+        flingContainer.setAdapter(arrayAdapter);
+
+        // Swipe Listener
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(MainActivity.this, "Left!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(MainActivity.this, "Right!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+                //View view = flingContainer.getSelectedView();
+                //view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
+                //view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(MainActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //Menu to go to profile page
         Button menuButton = findViewById(R.id.menuButton);
@@ -60,4 +139,22 @@ public class MainActivity extends AppCompatActivity {
         });
         filtersDialog.show();
     }
+
+    // For SwipeCards until line (150) ***EDIT***
+    /*
+    static void makeToast(Context ctx, String s){
+        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.right)
+    public void right() {
+        *//**
+         * Trigger the right event manually.
+         *//*
+        flingContainer.getTopCardListener().selectRight();
+    }
+    @OnClick(R.id.left)
+    public void left() {
+        flingContainer.getTopCardListener().selectLeft();
+    }
+    */
 }
