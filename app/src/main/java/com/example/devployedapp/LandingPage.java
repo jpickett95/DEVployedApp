4109d5c7452b9a230f956acd56c9460f527561a4
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2022-10-05 16:09:20 -0500

    Created new class for the LandingPage. Imported DEVployed logo image which created many image assets in different shapes. Began working on the layout for the LandingPage.

---
+++ app/src/main/java/com/example/devployedapp/LandingPage.java
@@ -0,0 +1,12 @@
+package com.example.devployedapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+}
