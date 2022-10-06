cb08c9a942ff81a901df08ccfbcea01eb314ea7b
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2022-10-06 11:44:48 -0500

    LandingPage.java creation complete. Implemented as the first screen to be loaded when app launches.
    Need to redesign and implement different toolbar system - currently it loads as Tyrian Purple bar showing "DEVployed App", then a second toolbar with the exit/back button loads below it before the actual page itself is shown. Need to combine these two bars or eliminate one.

--- app/src/main/java/com/example/devployedapp/LandingPage.java
+++ app/src/main/java/com/example/devployedapp/LandingPage.java
@@ -1,6 +1,10 @@
 package com.example.devployedapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,4 +13,36 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+
+        Button reviewJobMatchesButton = findViewById(R.id.review_job_matches_button);
+        reviewJobMatchesButton.setOnClickListener(view -> startActivity(new Intent(LandingPage.this, MainActivity.class)));
+        Button toSavedJobsList = findViewById(R.id.saved_matches_button);
+        toSavedJobsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LandingPage.this, SavedJobsListPage.class));
+            }
+        });
+        Button toRejectedJobsList = findViewById(R.id.rejected_matches_button);
+        toRejectedJobsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LandingPage.this, RejectedJobsListPage.class));
+            }
+        });
+        Button toProfilePage = findViewById(R.id.to_profile_page_button);
+        toProfilePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LandingPage.this, ProfilePage.class));
+            }
+        });
+        //toolbar exit button to exit the app
+        ImageButton backButton_main = findViewById(R.id.exitButton_main2);
+        backButton_main.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
