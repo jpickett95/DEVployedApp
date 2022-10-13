8a6fb1eecf3e0be39eb8305a2e75ec3c94f41d1d
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-10-13 14:29:29 -0400

    Implemented shared preferences for profile page (except for profile image & programming languages);

--- app/src/main/java/com/example/devployedapp/ProfilePage.java
+++ app/src/main/java/com/example/devployedapp/ProfilePage.java
@@ -3,6 +3,7 @@ package com.example.devployedapp;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -33,10 +34,75 @@ public class ProfilePage extends AppCompatActivity implements LanguageDialog.Lan
     private EditText profileEmail;
     private EditText profilePhone;
     private ChipGroup educationalExperience;
+    private Chip highSchoolEDUChip;
+    private Chip associatesEDUChip;
+    private Chip bachelorsEDUChip;
+    private Chip mastersEDUChip;
+    private Chip phdEDUChip;
+    private Chip bootcampEDUChip;
     private ChipGroup programmingLanguages;
     private ChipGroup experienceLevel;
+    private Chip noexpEXPChip;
+    private Chip earlyEXPChip;
+    private Chip midEXPChip;
+    private Chip seniorEXPChip;
+    private Chip executiveEXPChip;
     private ChipGroup industryPreferences;
+    private Chip softwareDevINDChip;
+    private Chip gameDevINDChip;
+    private Chip itINDChip;
+    private Chip uxuiINDChip;
+    private Chip frontEndINDChip;
+    private Chip backEndINDChip;
+    private Chip fullStackINDChip;
     private ImageView profileImage;
+    private Button saveProfileButton;
+
+    // Shared Preferences Constants
+    public static final String SHARED_PREFERENCES = "sharedPreferences";
+    public static final String PROFILE_NAME = "profileName";
+    public static final String PROFILE_EMAIL = "profileEmail";
+    public static final String PROFILE_PHONE = "profilePhone";
+    public static final String PROFILE_IMAGE = "profileImage";
+    public static final String PROFILE_EDU_HIGHSCHOOL = "profileEduHS";
+    public static final String PROFILE_EDU_ASSOCIATES = "profileEduAS";
+    public static final String PROFILE_EDU_BACHELORS = "profileEduBS";
+    public static final String PROFILE_EDU_MASTERS = "profileEduMS";
+    public static final String PROFILE_EDU_PHD = "profileEduPHD";
+    public static final String PROFILE_EDU_BOOTCAMP = "profileEduBootcamp";
+    public static final String PROFILE_EXP_NOEXP = "profileExpNoExp";
+    public static final String PROFILE_EXP_EARLY = "profileExpEarly";
+    public static final String PROFILE_EXP_MID = "profileExpMid";
+    public static final String PROFILE_EXP_SENIOR = "profileExpSenior";
+    public static final String PROFILE_EXP_EXECUTIVE = "profileExpExecutive";
+    public static final String PROFILE_INDUSTRY_SOFTWAREDEV = "profileIndustrySoftwareDev";
+    public static final String PROFILE_INDUSTRY_GAMEDEV = "profileIndustryGameDev";
+    public static final String PROFILE_INDUSTRY_IT = "profileIndustryIT";
+    public static final String PROFILE_INDUSTRY_UXUI = "profileIndustryUXUI";
+    public static final String PROFILE_INDUSTRY_FRONTEND = "profileIndustryFrontEnd";
+    public static final String PROFILE_INDUSTRY_BACKEND = "profileIndustryBackEnd";
+    public static final String PROFILE_INDUSTRY_FULLSTACK = "profileIndustryFullStack";
+    private String fullName;
+    private String email;
+    private String phone;
+    private boolean highSchoolOnOff;
+    private boolean associatesOnOff;
+    private boolean bachelorsOnOff;
+    private boolean mastersOnOff;
+    private boolean phdOnOff;
+    private boolean bootcampOnOff;
+    private boolean noexpOnOff;
+    private boolean earlyexpOnOff;
+    private boolean midexpOnOff;
+    private boolean seniorexpOnOff;
+    private boolean executiveOnOff;
+    private boolean softwaredevOnOff;
+    private boolean gamedevOnOff;
+    private boolean itOnOff;
+    private boolean uxuiOnOff;
+    private boolean fullstackOnOff;
+    private boolean frontendOnOff;
+    private boolean backendOnOff;
 
     private static final int IMAGE_PICK_CODE = 1000;
     private static final int PERMISSION_CODE = 1001;
@@ -53,10 +119,29 @@ public class ProfilePage extends AppCompatActivity implements LanguageDialog.Lan
         profileEmail = findViewById(R.id.editTextEmailAddress);
         profilePhone = findViewById(R.id.editTextPhone);
         educationalExperience = findViewById(R.id.ChipGroup_education);
+        highSchoolEDUChip = findViewById(R.id.chip_profile_highschool);
+        associatesEDUChip = findViewById(R.id.chip_profile_associates);
+        bachelorsEDUChip = findViewById(R.id.chip_profile_bachelors);
+        mastersEDUChip = findViewById(R.id.chip_profile_masters);
+        phdEDUChip = findViewById(R.id.chip_profile_phd);
+        bootcampEDUChip = findViewById(R.id.chip_profile_bootcamp);
         programmingLanguages = findViewById(R.id.ChipGroup_profile_programmingLanguages);
         experienceLevel = findViewById(R.id.ChipGroup_profile_experience);
+        noexpEXPChip = findViewById(R.id.chip_profile_noexp);
+        earlyEXPChip = findViewById(R.id.chip_profile_earlycareer);
+        midEXPChip = findViewById(R.id.chip_profile_midcareer);
+        seniorEXPChip = findViewById(R.id.chip_profile_senior);
+        executiveEXPChip = findViewById(R.id.chip_profile_executive);
         industryPreferences = findViewById(R.id.ChipGroup_profile_industrypreferences);
+        softwareDevINDChip = findViewById(R.id.chip_profile_softwaredev);
+        gameDevINDChip = findViewById(R.id.chip_profile_gamedev);
+        itINDChip = findViewById(R.id.chip_profile_it);
+        uxuiINDChip = findViewById(R.id.chip_profile_uxui);
+        frontEndINDChip = findViewById(R.id.chip_profile_frontend);
+        backEndINDChip = findViewById(R.id.chip_profile_backend);
+        fullStackINDChip = findViewById(R.id.chip_profile_fullstack);
         profileImage = findViewById(R.id.profileImage);
+        saveProfileButton = findViewById(R.id.saveProfileButton);
 
         // Add new programming languages
         addLanguageButton = findViewById(R.id.programmingLanguages_addButton);
@@ -100,8 +185,112 @@ public class ProfilePage extends AppCompatActivity implements LanguageDialog.Lan
             }
         });
 
+        // Save Profile Button click handler
+        saveProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+
         // Dialog Boxes
         skillsDialog = new Dialog(this);
+
+        // Shared Preferences
+        loadData();
+        updateViews();
+    }
+
+    // For Shared Preferences
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(PROFILE_NAME, profileFullName.getText().toString());
+        editor.putString(PROFILE_EMAIL, profileEmail.getText().toString());
+        editor.putString(PROFILE_PHONE, profilePhone.getText().toString());
+        editor.putBoolean(PROFILE_EDU_HIGHSCHOOL, highSchoolEDUChip.isChecked());
+        editor.putBoolean(PROFILE_EDU_ASSOCIATES, associatesEDUChip.isChecked());
+        editor.putBoolean(PROFILE_EDU_BACHELORS, bachelorsEDUChip.isChecked());
+        editor.putBoolean(PROFILE_EDU_MASTERS, mastersEDUChip.isChecked());
+        editor.putBoolean(PROFILE_EDU_PHD, phdEDUChip.isChecked());
+        editor.putBoolean(PROFILE_EDU_BOOTCAMP, bootcampEDUChip.isChecked());
+        editor.putBoolean(PROFILE_EXP_NOEXP, noexpEXPChip.isChecked());
+        editor.putBoolean(PROFILE_EXP_EARLY, earlyEXPChip.isChecked());
+        editor.putBoolean(PROFILE_EXP_MID, midEXPChip.isChecked());
+        editor.putBoolean(PROFILE_EXP_SENIOR, seniorEXPChip.isChecked());
+        editor.putBoolean(PROFILE_EXP_EXECUTIVE, executiveEXPChip.isChecked());
+        editor.putBoolean(PROFILE_INDUSTRY_BACKEND, backEndINDChip.isChecked());
+        editor.putBoolean(PROFILE_INDUSTRY_SOFTWAREDEV, softwareDevINDChip.isChecked());
+        editor.putBoolean(PROFILE_INDUSTRY_GAMEDEV, gameDevINDChip.isChecked());
+        editor.putBoolean(PROFILE_INDUSTRY_FRONTEND, frontEndINDChip.isChecked());
+        editor.putBoolean(PROFILE_INDUSTRY_FULLSTACK, fullStackINDChip.isChecked());
+        editor.putBoolean(PROFILE_INDUSTRY_IT, itINDChip.isChecked());
+        editor.putBoolean(PROFILE_INDUSTRY_UXUI, uxuiINDChip.isChecked());
+
+        editor.apply();
+
+        Toast.makeText(this, "Profile Saved!", Toast.LENGTH_SHORT).show();
+    }
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        // (variable) = sharedPreferences.getString(CONSTANT, default value "") --- OR ...getBoolean(CONSTANT, default value)
+
+        // Text
+        fullName = sharedPreferences.getString(PROFILE_NAME, "");
+        email = sharedPreferences.getString(PROFILE_EMAIL, "");
+        phone = sharedPreferences.getString(PROFILE_PHONE, "");
+
+        // Booleans
+        highSchoolOnOff = sharedPreferences.getBoolean(PROFILE_EDU_HIGHSCHOOL, false);   // Education
+        associatesOnOff = sharedPreferences.getBoolean(PROFILE_EDU_ASSOCIATES, false);
+        bachelorsOnOff = sharedPreferences.getBoolean(PROFILE_EDU_BACHELORS, false);
+        mastersOnOff = sharedPreferences.getBoolean(PROFILE_EDU_MASTERS, false);
+        phdOnOff = sharedPreferences.getBoolean(PROFILE_EDU_PHD, false);
+        bootcampOnOff = sharedPreferences.getBoolean(PROFILE_EDU_BOOTCAMP, false);
+        noexpOnOff = sharedPreferences.getBoolean(PROFILE_EXP_NOEXP, false);  // Experience
+        earlyexpOnOff = sharedPreferences.getBoolean(PROFILE_EXP_EARLY, false);
+        midexpOnOff = sharedPreferences.getBoolean(PROFILE_EXP_MID, false);
+        seniorexpOnOff = sharedPreferences.getBoolean(PROFILE_EXP_SENIOR, false);
+        executiveOnOff = sharedPreferences.getBoolean(PROFILE_EXP_EXECUTIVE, false);
+        softwaredevOnOff = sharedPreferences.getBoolean(PROFILE_INDUSTRY_SOFTWAREDEV, false);   // Industry
+        gamedevOnOff = sharedPreferences.getBoolean(PROFILE_INDUSTRY_GAMEDEV, false);
+        itOnOff = sharedPreferences.getBoolean(PROFILE_INDUSTRY_IT, false);
+        uxuiOnOff = sharedPreferences.getBoolean(PROFILE_INDUSTRY_UXUI, false);
+        fullstackOnOff = sharedPreferences.getBoolean(PROFILE_INDUSTRY_FULLSTACK, false);
+        frontendOnOff = sharedPreferences.getBoolean(PROFILE_INDUSTRY_FRONTEND, false);
+        backendOnOff = sharedPreferences.getBoolean(PROFILE_INDUSTRY_BACKEND, false);
+
+    }
+    public void updateViews(){
+        // Text
+        profileFullName.setText(fullName);
+        profileEmail.setText(email);
+        profilePhone.setText(phone);
+
+        // Education
+        highSchoolEDUChip.setChecked(highSchoolOnOff);
+        associatesEDUChip.setChecked(associatesOnOff);
+        bachelorsEDUChip.setChecked(bachelorsOnOff);
+        mastersEDUChip.setChecked(mastersOnOff);
+        phdEDUChip.setChecked(phdOnOff);
+        bootcampEDUChip.setChecked(bootcampOnOff);
+
+        // Experience
+        noexpEXPChip.setChecked(noexpOnOff);
+        earlyEXPChip.setChecked(earlyexpOnOff);
+        midEXPChip.setChecked(midexpOnOff);
+        seniorEXPChip.setChecked(seniorexpOnOff);
+        executiveEXPChip.setChecked(executiveOnOff);
+
+        // Industries
+        softwareDevINDChip.setChecked(softwaredevOnOff);
+        gameDevINDChip.setChecked(gamedevOnOff);
+        itINDChip.setChecked(itOnOff);
+        uxuiINDChip.setChecked(uxuiOnOff);
+        frontEndINDChip.setChecked(frontendOnOff);
+        backEndINDChip.setChecked(backendOnOff);
+        fullStackINDChip.setChecked(fullstackOnOff);
     }
 
     // Used for profile image long-click
