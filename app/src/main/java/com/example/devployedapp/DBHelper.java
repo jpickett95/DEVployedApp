ee22cbb645e25ba111263d81b1eb5467eace8bb8
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-10-25 18:42:58 -0400

    Added comments;

--- app/src/main/java/com/example/devployedapp/DBHelper.java
+++ app/src/main/java/com/example/devployedapp/DBHelper.java
@@ -1,14 +1,23 @@
 package com.example.devployedapp;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
+/*-- This table will record ALL of the jobs that the user rejects/saves. It will just
+        -- give the ability to sort by that distinction (saved/rejected)
+        CREATE TABLE IF NOT EXISTS all_jobs (
+        job_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        job_title TEXT NOT NULL, -- As job names can often be long (this will be limited when I know the average length of a job title)
+        job_status VARCHAR[8] NOT NULL, -- saved | rejected | applied | unseen | seen
+        job_location VARCHAR[100] NOT NULL,
+        job_company_name VARCHAR[20] NOT NULL,
+        additional_information TEXT NOT NULL -- This will be a json string, that will need to be de-serialized when accessed
+        );
 
-import com.example.webparser.data.JobListing;
+        CREATE VIEW saved_jobs AS SELECT * FROM all_jobs WHERE job_status = 'saved';
+        CREATE VIEW rejected_jobs AS SELECT * FROM all_jobs WHERE job_status = 'rejected';*/
+
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -46,4 +55,18 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
         DB.execSQL( "DROP TABLE IF EXISTS " + DATABASE_TABLE);
     }
+
 }
+
+// For a 'settings_table' database
+/*
+    CREATE TABLE IF NOT EXISTS  app_settings (
+        setting_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        setting_name VARCHAR[20] NOT NULL,
+        setting_type VARCHAR[10] NOT NULL, -- bool, integer, float (any raw data type)
+
+    setting_value BLOB NOT NULL
+    -- this is a raw save of the information given, we will
+            -- require that the setting_type be filled out in order
+            -- to know what type of data to return (at runtime)
+            );*/
